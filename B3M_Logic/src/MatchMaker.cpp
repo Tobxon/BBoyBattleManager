04e98b8cd8817e165271a82dd1437153898e4b1d
Tobxon <tobxon@googlemail.com>
2023-09-08 22:45:00 +0200

    extracted function and related cleanups
    Created reorderContestantsByIterateOrderToProjection function.
    Got rid of contestantsElement_t because its actually just Contestant.

--- B3M_Logic/src/MatchMaker.cpp
+++ B3M_Logic/src/MatchMaker.cpp
@@ -39,6 +39,7 @@ using b3m::common::Match;
 
 
 void sortTeamsByResults(std::vector< Contestant >&, const History&);
+std::vector< std::reference_wrapper< const Contestant >> reorderContestantsByIterateOrder(const std::vector< Contestant >&, const History&);
 
 
 //--------------------------------------------------------------------------------------------------
@@ -52,7 +53,6 @@ auto b3m::logic::SwissMatchMaker::createRound(const Tournament& i_tournament) ->
 	{
 		return {};
 	}
-	using contestantsElement_t = decltype(contestants)::value_type;
 
 	TournamentRound o_round;
 
@@ -61,7 +61,7 @@ auto b3m::logic::SwissMatchMaker::createRound(const Tournament& i_tournament) ->
 	if(history.empty())
 	{
 		//sort contestants by initial ranking
-		std::ranges::sort(contestants, [](const contestantsElement_t& i_a, const contestantsElement_t i_b){
+		std::ranges::sort(contestants, [](const Contestant& i_a, const Contestant i_b){
 			return i_a.getRating() < i_b.getRating();
 		});
 
@@ -78,46 +78,17 @@ auto b3m::logic::SwissMatchMaker::createRound(const Tournament& i_tournament) ->
 		//sort contestants by match results (and initial rating)
 		sortTeamsByResults(contestants, history);
 
-		//iterate over contestants - first those who have waited in rounds before and then from by ranking
-		std::vector< std::reference_wrapper< const contestantsElement_t >> contestantsIterateOrder;
-		contestantsIterateOrder.reserve(contestants.size());
-
-		for(const auto& round : std::views::reverse(history))
-		{
-			if(round)
-			{
-				//TODO assumes that only one contestant didn't match last round - with the current from contestant perspective it can happen that multiple don't find a match
-				if(const auto contestantWaited = std::ranges::find_if_not(contestants, [&round](const Contestant& i_contestant){
-					return std::ranges::find_if(*round, [&i_contestant](const Match& i_match){
-						const auto& opponents = i_match.getContestantNames();
-						return opponents.first == i_contestant.getName() || opponents.second == i_contestant.getName();
-					}) != round->cend();
-				}); contestantWaited != contestants.cend())
-				{
-					contestantsIterateOrder.emplace_back(*contestantWaited);
-				}
-			}
-		}
-		for(const auto& contestant : contestants)
-		{
-			if(std::ranges::find_if(contestantsIterateOrder, [contestant](const auto& contestantReference){
-				return contestantReference.get().getName() == contestant.getName();
-			}) == contestantsIterateOrder.cend())
-			{
-				contestantsIterateOrder.emplace_back(contestant);
-			}
-		}
-
-	//	std::unordered_map< std::reference_wrapper< const contestantsElement_t >, bool, std::hash<Contestant>, std::equal< Contestant >> takenContestants;
-		std::map< std::reference_wrapper< const contestantsElement_t >, bool, std::function<bool(const contestantsElement_t&, const contestantsElement_t&)> > takenContestants{
-			[](const contestantsElement_t& i_a, const contestantsElement_t& i_b)
+		//TODO improvement by using unordered_map?
+		std::map< std::reference_wrapper< const Contestant >, bool, std::function<bool(const Contestant&, const Contestant&)> > takenContestants{
+			[](const Contestant& i_a, const Contestant& i_b)
 			{
 				return i_a.getName() < i_b.getName();
 			}
 		};
-		std::ranges::transform(contestantsIterateOrder, std::inserter(takenContestants, takenContestants.end()), [](const auto& i_contestantRef){ return std::make_pair(i_contestantRef, false); });
+		std::ranges::transform(contestants, std::inserter(takenContestants, takenContestants.end()), [](const auto& i_contestantRef){ return std::make_pair(i_contestantRef, false); });
 
-		for(const auto& currentContestantRef : contestantsIterateOrder)
+		//iterate over contestants - first those who have waited in rounds before and then by ranking
+		for(const auto& currentContestantRef : reorderContestantsByIterateOrder(contestants, history))
 		{
 			if(!takenContestants.at(currentContestantRef))
 			{
@@ -150,10 +121,10 @@ auto b3m::logic::SwissMatchMaker::createRound(const Tournament& i_tournament) ->
 
 				//remove itself and previous opponents from possible opponents list
 				auto possibleOpponents = contestants
-					| std::views::filter([&currentContestantRef](const contestantsElement_t& i_contestant){ return i_contestant != currentContestantRef.get(); })
-					| std::views::filter([&takenContestants](const contestantsElement_t& i_contestant){ return !takenContestants.at(i_contestant); })
-					| std::views::filter([&previousOpponents](const contestantsElement_t& i_contestant){ return std::ranges::find_if(previousOpponents, [&i_contestant](const auto& i_contestantRef){ return i_contestantRef.get() == i_contestant; }) == previousOpponents.end(); })
-					| std::ranges::to<std::vector<std::reference_wrapper< const contestantsElement_t >>>();
+					| std::views::filter([&currentContestantRef](const Contestant& i_contestant){ return i_contestant != currentContestantRef.get(); })
+					| std::views::filter([&takenContestants](const Contestant& i_contestant){ return !takenContestants.at(i_contestant); })
+					| std::views::filter([&previousOpponents](const Contestant& i_contestant){ return std::ranges::find_if(previousOpponents, [&i_contestant](const auto& i_contestantRef){ return i_contestantRef.get() == i_contestant; }) == previousOpponents.end(); })
+					| std::ranges::to<std::vector<std::reference_wrapper< const Contestant >>>();
 
 				if(!possibleOpponents.empty())
 				{
@@ -169,8 +140,8 @@ auto b3m::logic::SwissMatchMaker::createRound(const Tournament& i_tournament) ->
 					}
 					contestantsWeightingOrder.insert(contestantsWeightingOrder.end(), contestants.cbegin(), contestants.cbegin() + curContestantPos);
 
-					std::map< std::reference_wrapper< const decltype(contestants)::value_type >, int, std::function<bool(const contestantsElement_t&, const contestantsElement_t&)> > contestantsWeighting{
-						[](const contestantsElement_t& i_a, const contestantsElement_t& i_b)
+					std::map< std::reference_wrapper< const decltype(contestants)::value_type >, int, std::function<bool(const Contestant&, const Contestant&)> > contestantsWeighting{
+						[](const Contestant& i_a, const Contestant& i_b)
 						{
 							return i_a.getName() < i_b.getName();
 						}
@@ -243,7 +214,6 @@ void sortTeamsByResults(std::vector< Contestant >& i_contestantsToSort, const Hi
 		[&contestantsRating = std::as_const(contestantsRating),
 		 &numberOfRounds = std::as_const(numberOfRounds)]
 		(const Contestant& i_a, const Contestant& i_b){
-//			return contestantsRating.at(i_a.getName()) > contestantsRating.at(i_b.getName());
 			const auto combRatingA = contestantsRating.at(i_a.getName()).getCombinedRating();
 			const auto combRatingB = contestantsRating.at(i_b.getName()).getCombinedRating();
 			if(combRatingA == combRatingB)
@@ -253,6 +223,43 @@ void sortTeamsByResults(std::vector< Contestant >& i_contestantsToSort, const Hi
 			return static_cast<double>(combRatingA)/numberOfRounds
 				> static_cast<double>(combRatingB)/numberOfRounds;
 	});
+}
+
+std::vector< std::reference_wrapper< const Contestant >> reorderContestantsByIterateOrder(const std::vector< Contestant >& i_contestantsToReorder, const History& i_history)
+{
+	std::vector< std::reference_wrapper< const Contestant >> o_reorderedContestants;
+	o_reorderedContestants.reserve(i_contestantsToReorder.size());
+
+	for(const auto& round : std::views::reverse(i_history))
+	{
+		if(round)
+		{
+			//TODO assumes that only one contestant didn't match last round - with the current from contestant perspective it can happen that multiple don't find a match
+			if(const auto contestantWaited = std::ranges::find_if_not(i_contestantsToReorder,
+				[&round](const Contestant& i_contestant){
+					return std::ranges::find_if(*round, [&i_contestant](const Match& i_match){
+						const auto& opponents = i_match.getContestantNames();
+						return opponents.first == i_contestant.getName() ||
+							opponents.second == i_contestant.getName();
+					}) != round->cend();
+			}); contestantWaited != i_contestantsToReorder.cend())
+			{
+				o_reorderedContestants.emplace_back(*contestantWaited);
+			}
+		}
+	}
+	for(const auto& contestant : i_contestantsToReorder)
+	{
+		if(std::ranges::find_if(o_reorderedContestants,
+			[contestant](const auto& contestantReference){
+			return contestantReference.get().getName() == contestant.getName();
+		}) == o_reorderedContestants.cend())
+		{
+			o_reorderedContestants.emplace_back(contestant);
+		}
+	}
+
+	return o_reorderedContestants;
 }
 
 

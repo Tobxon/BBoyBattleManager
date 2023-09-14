268238e4eaf267ec244ebc8ffa531198fa90b8be
Tobxon <tobxon@e.mail.de>
2023-09-14 19:01:56 +0200

    state of work - ranking slide

--- B3M_GUI/src/ExpressYourself2023/RankingSlideSelector.cpp
+++ B3M_GUI/src/ExpressYourself2023/RankingSlideSelector.cpp
@@ -15,6 +15,13 @@
 
 
 //--------------------------------------------------------------------------------------------------
+//------ Declarations                                                                         ------
+//--------------------------------------------------------------------------------------------------
+static constexpr /*unsigned*/ int widthScreen = 1920;
+static constexpr /*unsigned*/ int heightScreen = 1080;
+
+
+//--------------------------------------------------------------------------------------------------
 //------ Implementations                                                                      ------
 //--------------------------------------------------------------------------------------------------
 
@@ -25,27 +32,78 @@ b3m::gui::ey2023::RankingSlide::RankingSlide(QWidget* i_parent)
 	m_ui->setupUi(this);
 
 	//TODO import font at a central place
-	int id = QFontDatabase::addApplicationFont("D:/Dev/Repos/B3M_dev/EY2023/resources/freshmarker.ttf");
+	int id = QFontDatabase::addApplicationFont("C:/Dev/Repos/B3M_dev/EY2023/resources/freshmarker.ttf");
 	QString family = QFontDatabase::applicationFontFamilies(id).at(0);
 	QFont freshmarker(family);
 
-	QRect centralSpace{ 250, 100, 1420, 880 };
+	QFont system;
 
 	QList< QString > teamNames{ "DDC Talents", "GingerBreads Crew", "Style Insiders", "Nasty Attack", "Offbeat", "Dream ASF", "Crazy Monkeys", "Fresh Family", "Speed Breaker", "The Wild Foxes", "Die BBoys", "Fire Girls" };
-	int heightOfSingleName = centralSpace.height()/teamNames.size();
-	freshmarker.setPointSize(heightOfSingleName > 200 ? 100 : heightOfSingleName/2);
+	int heightOfRow = (heightScreen/10*8)/(teamNames.size()+1);
+	const auto fontSize = heightOfRow > 200 ? 100 : heightOfRow/2;
+	freshmarker.setPointSize(fontSize);
+	system.setPointSize(fontSize);
 	int index = 0;
+	{
+		// #
+		QString rankingHeaderLettering{"#"};
+		auto* const rankingHeader = new QLabel(this);
+		rankingHeader->setObjectName(rankingHeaderLettering);
+		QRect rankingHeaderSpace{ widthScreen/10, heightScreen/10, heightOfRow, heightOfRow };
+		rankingHeader->setGeometry(rankingHeaderSpace);
+		rankingHeader->setMidLineWidth(0);
+		rankingHeader->setText(rankingHeaderLettering);
+		rankingHeader->setAlignment(Qt::AlignCenter);
+		rankingHeader->setFont(system);
+		rankingHeader->setAttribute( Qt::WA_TranslucentBackground, true );
+
+		// Crew
+		QString crewHeaderLettering{"Crew"};
+		auto* const crewHeader = new QLabel(this);
+		crewHeader->setObjectName(crewHeaderLettering);
+		QRect crewHeaderSpace{ widthScreen/10+heightOfRow, heightScreen/10, widthScreen/2-widthScreen/10+heightOfRow, heightOfRow };
+		crewHeader->setGeometry(crewHeaderSpace);
+		crewHeader->setMidLineWidth(0);
+		crewHeader->setText(crewHeaderLettering);
+		crewHeader->setAlignment(Qt::AlignCenter);
+		crewHeader->setFont(system);
+		crewHeader->setAttribute( Qt::WA_TranslucentBackground, true );
+
+		// Siege
+		QString winsHeaderLettering{"Siege"};
+		auto* const winsHeader = new QLabel(this);
+		winsHeader->setObjectName(winsHeaderLettering);
+		QRect winsHeaderSpace{ widthScreen/2, heightScreen/10, (widthScreen/2-widthScreen/10)/2, heightOfRow };
+		winsHeader->setGeometry(winsHeaderSpace);
+		winsHeader->setMidLineWidth(0);
+		winsHeader->setText(winsHeaderLettering);
+		winsHeader->setAlignment(Qt::AlignCenter);
+		winsHeader->setFont(system);
+		winsHeader->setAttribute( Qt::WA_TranslucentBackground, true );
+
+		// Hände
+		QString pointsHeaderLettering{"Siege"};
+		auto* const pointsHeader = new QLabel(this);
+		pointsHeader->setObjectName(pointsHeaderLettering);
+		QRect pointsHeaderSpace{ widthScreen/2 + (widthScreen/2-widthScreen/10)/2, heightScreen/10, (widthScreen/2-widthScreen/10)/2, heightOfRow };
+		pointsHeader->setGeometry(pointsHeaderSpace);
+		pointsHeader->setMidLineWidth(0);
+		pointsHeader->setText(winsHeaderLettering);
+		pointsHeader->setAlignment(Qt::AlignCenter);
+		pointsHeader->setFont(system);
+		pointsHeader->setAttribute( Qt::WA_TranslucentBackground, true );
+	}
 	for(const auto teamName : teamNames)
 	{
-		auto* const teamNameLabel = new QLabel(this);
-		teamNameLabel->setObjectName(teamName);
-		QRect curSpace{ centralSpace.left(), centralSpace.top()+heightOfSingleName*index, centralSpace.width(), heightOfSingleName };
-		teamNameLabel->setGeometry(curSpace);
-		teamNameLabel->setMidLineWidth(0);
-		teamNameLabel->setText(teamName);
-		teamNameLabel->setAlignment(Qt::AlignCenter);
-		teamNameLabel->setFont(freshmarker);
-		teamNameLabel->setAttribute( Qt::WA_TranslucentBackground, true );
+//		auto* const teamNameLabel = new QLabel(this);
+//		teamNameLabel->setObjectName(teamName);
+//		QRect curSpace{ centralSpace.left(), centralSpace.top()+heightOfRow*index, centralSpace.width(), heightOfRow };
+//		teamNameLabel->setGeometry(curSpace);
+//		teamNameLabel->setMidLineWidth(0);
+//		teamNameLabel->setText(teamName);
+//		teamNameLabel->setAlignment(Qt::AlignCenter);
+//		teamNameLabel->setFont(freshmarker);
+//		teamNameLabel->setAttribute( Qt::WA_TranslucentBackground, true );
 		++index;
 	}
 //	arrangeTeamNames();

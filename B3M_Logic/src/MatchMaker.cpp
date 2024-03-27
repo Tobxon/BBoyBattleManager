1d7cb2daf84e2b46835b07c1cf73657f0ecb69e8
Tobxon <tobxon@e.mail.de>
2024-03-27 10:25:09 +0100

    fix creating tournament rounds after the first one
    and additional some cleanups in surrounding code

--- B3M_Logic/src/MatchMaker.cpp
+++ B3M_Logic/src/MatchMaker.cpp
@@ -47,6 +47,15 @@ std::vector< ContestantRef_t > getPreviousOpponents(const Contestant&, const std
 //------ Implementations                                                                      ------
 //--------------------------------------------------------------------------------------------------
 
+auto b3m::logic::MatchMaker::getCurrentRanking(const Tournament& i_tournament) const -> SortedContestantsRanking
+{
+	auto contestants = i_tournament.getContestants();
+	const auto& history = i_tournament.getHistory();
+
+	return getSortedRanking(history, contestants);
+}
+
+//SwissMatchMaker ----------------------------------------------------------------------------------
 auto b3m::logic::SwissMatchMaker::createRound(const Tournament& i_tournament) -> TournamentRound
 {
 	auto contestants = i_tournament.getContestants();
@@ -56,7 +65,6 @@ auto b3m::logic::SwissMatchMaker::createRound(const Tournament& i_tournament) ->
 	}
 
 	const auto& history = i_tournament.getHistory();
-
 
 	TournamentRound o_round{ i_tournament, "Runde " + std::to_string(++m_roundCount) };
 	if(history.empty())
@@ -79,56 +87,46 @@ auto b3m::logic::SwissMatchMaker::createRound(const Tournament& i_tournament) ->
 	sortTeamsByResults(contestants, history);
 
 	//iterate over contestants - first those who have waited in rounds before and then by ranking
-	const auto contestantsInIterateOrder = reorderContestantsByPriority(contestants, history);
-	for(const auto& currentContestantRef : contestantsInIterateOrder)
+	const std::vector< ContestantRef_t > contestantsInIterateOrder = reorderContestantsByPriority(contestants, history);
+	for(const ContestantRef_t& currentContestantRef : contestantsInIterateOrder)
 	{
-		const auto& checkIfContestantIsAvailable = [&curRound = /*std::as_const(*/o_round/*)*/](const Contestant& i_contestant){
-			return !doesContestantParticipateInRound(curRound, i_contestant);
-		};
-
-		if(!checkIfContestantIsAvailable(currentContestantRef))
+		if(!doesContestantParticipateInRound(o_round, currentContestantRef))
 		{
 			//remove itself and previous opponents from possible opponents list
 			auto possibleOpponents = contestants
 				| std::views::filter([&currentContestantRef](const Contestant& i_contestant){ return i_contestant != currentContestantRef.get(); })
-				| std::views::filter(checkIfContestantIsAvailable)
-				| std::views::filter([previousOpponents = getPreviousOpponents(currentContestantRef.get(), contestants, history)](const Contestant& i_contestant){ return std::ranges::find_if(previousOpponents, [&i_contestant](const auto& i_contestantRef){ return i_contestantRef.get() == i_contestant; }) == previousOpponents.end(); })
+				| std::views::filter([&curRound = /*std::as_const(*/o_round/*)*/](const Contestant& i_contestant){ return !doesContestantParticipateInRound(curRound, i_contestant); })
+				| std::views::filter([previousOpponents = getPreviousOpponents(currentContestantRef.get(), contestants, history)](const Contestant& i_contestant){
+					return std::ranges::find_if(previousOpponents, [&i_contestant](const ContestantRef_t& i_contestantRef){ return i_contestantRef.get() == i_contestant; }) == previousOpponents.end(); })
 				| std::ranges::to<std::vector<ContestantRef_t>>();
 
 			if(!possibleOpponents.empty())
 			{
 				//sort possible opponents by distance weight - wrapped around, perfect opponent gets 0 (like algorithm when history is empty), each element moving away -1)
-				const auto& curContestantPos = std::distance(contestants.begin(), std::ranges::find(contestants, currentContestantRef.get()));
-				std::vector< ContestantRef_t> contestantsWeightingOrder(contestants.cbegin() + curContestantPos, contestants.cend()); //TODO dont copy, create a view
-				const Contestant* blockerContestant{ nullptr };
+				const auto curContestantPos = std::distance(contestants.begin(), std::ranges::find(contestants, currentContestantRef.get()));
+				std::vector< ContestantRef_t> contestantsWeightingOrder(contestants.cbegin() + curContestantPos, contestants.cend());
+				std::unique_ptr< Contestant > blockerContestant{};
 				if(contestants.size() % 2 != 0)
 				{
-					blockerContestant = new Contestant(""); //TODO save enough?
+					blockerContestant = std::make_unique<Contestant>("");
 					contestantsWeightingOrder.emplace_back(*blockerContestant);
 				}
 				contestantsWeightingOrder.insert(contestantsWeightingOrder.end(), contestants.cbegin(), contestants.cbegin() + curContestantPos);
 
 				std::map< ContestantRef_t, int, std::function<bool(const Contestant&, const Contestant&)> > contestantsWeighting{
-					[](const Contestant& i_a, const Contestant& i_b)
-					{
+					[](const Contestant& i_a, const Contestant& i_b){
 						return i_a.getName() < i_b.getName();
-					}
-				};
+				}};
 				for(auto it = contestantsWeightingOrder.cbegin(); it != contestantsWeightingOrder.cend(); ++it)
 				{
 					contestantsWeighting[*it] = -std::abs(std::distance(it, (contestantsWeightingOrder.cbegin() + contestantsWeightingOrder.size()/2)));
 				}
 
-				std::ranges::sort(possibleOpponents, [&contestantsWeighting](const auto& i_contestantA, const auto& i_contestantB){
+				std::ranges::sort(possibleOpponents, [&contestantsWeighting](const Contestant& i_contestantA, const Contestant& i_contestantB){
 					return contestantsWeighting.at(i_contestantA) > contestantsWeighting.at(i_contestantB);
 				});
 
 				o_round.addMatch(Match(currentContestantRef.get(), possibleOpponents.front().get()));
-
-				if(contestants.size() % 2 != 0)
-				{
-					delete blockerContestant;
-				}
 			}
 			else
 			{
@@ -141,15 +139,7 @@ auto b3m::logic::SwissMatchMaker::createRound(const Tournament& i_tournament) ->
 	return o_round;
 }
 
-auto b3m::logic::MatchMaker::getCurrentRanking(const Tournament& i_tournament) const -> SortedContestantsRanking
-{
-	auto contestants = i_tournament.getContestants();
-	const auto& history = i_tournament.getHistory();
-
-	return getSortedRanking(history, contestants);
-}
-
-
+//KOMatchMaker -------------------------------------------------------------------------------------
 b3m::logic::KOMatchMaker::KOMatchMaker(unsigned int i_numOfContestants)
 	: m_currentNumOfContestants(i_numOfContestants)
 {
